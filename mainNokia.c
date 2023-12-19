49c61d6537d32fbec1fd738bd1dc2ca34bb510b9
Sabrina Hsia <98360662+sabrinaahsiaa@users.noreply.github.com>
2023-12-18 22:14:16 -0800

    Update mainNokia.c
    Copyright for original source code

--- mainNokia.c
+++ mainNokia.c
@@ -1,3 +1,83 @@
+/*
+ * main.c - Example project for UT.6.02x Embedded Systems - Shape the World
+ * Jonathan Valvano and Ramesh Yerraballi
+ * March 1, 2015
+ * Hardware requirements 
+     TM4C123 LaunchPad, optional Nokia5110
+     CC3100 wifi booster and 
+     an internet access point with OPEN, WPA, or WEP security
+ 
+ * derived from TI's getweather example
+ * Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+*/
+
+/*
+ * Application Name     -   Get weather
+ * Application Overview -   This is a sample application demonstrating how to
+                            connect to openweathermap.org server and request for
+              weather details of a city.
+ * Application Details  -   http://processors.wiki.ti.com/index.php/CC31xx_SLS_Get_Weather_Application
+ *                          doc\examples\sls_get_weather.pdf
+ */
+ /* CC3100 booster pack connections (unused pins can be used by user application)
+Pin  Signal        Direction      Pin   Signal     Direction
+P1.1  3.3 VCC         IN          P2.1  Gnd   GND      IN
+P1.2  PB5 UNUSED      NA          P2.2  PB2   IRQ      OUT
+P1.3  PB0 UART1_TX    OUT         P2.3  PE0   SSI2_CS  IN
+P1.4  PB1 UART1_RX    IN          P2.4  PF0   UNUSED   NA
+P1.5  PE4 nHIB        IN          P2.5  Reset nRESET   IN
+P1.6  PE5 UNUSED      NA          P2.6  PB7  SSI2_MOSI IN
+P1.7  PB4 SSI2_CLK    IN          P2.7  PB6  SSI2_MISO OUT
+P1.8  PA5 UNUSED      NA          P2.8  PA4   UNUSED   NA
+P1.9  PA6 UNUSED      NA          P2.9  PA3   UNUSED   NA
+P1.10 PA7 UNUSED      NA          P2.10 PA2   UNUSED   NA
+
+Pin  Signal        Direction      Pin   Signal      Direction
+P3.1  +5  +5 V       IN           P4.1  PF2 UNUSED      OUT
+P3.2  Gnd GND        IN           P4.2  PF3 UNUSED      OUT
+P3.3  PD0 UNUSED     NA           P4.3  PB3 UNUSED      NA
+P3.4  PD1 UNUSED     NA           P4.4  PC4 UART1_CTS   IN
+P3.5  PD2 UNUSED     NA           P4.5  PC5 UART1_RTS   OUT
+P3.6  PD3 UNUSED     NA           P4.6  PC6 UNUSED      NA
+P3.7  PE1 UNUSED     NA           P4.7  PC7 NWP_LOG_TX  OUT
+P3.8  PE2 UNUSED     NA           P4.8  PD6 WLAN_LOG_TX OUT
+P3.9  PE3 UNUSED     NA           P4.9  PD7 UNUSED      IN (see R74)
+P3.10 PF1 UNUSED     NA           P4.10 PF4 UNUSED      OUT(see R75)
+
+UART0 (PA1, PA0) sends data to the PC via the USB debug cable, 115200 baud rate
+Port A, SSI0 (PA2, PA3, PA5, PA6, PA7) sends data to Nokia5110 LCD
+
+*/
 #include "..\cc3100\simplelink\include\simplelink.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
